f791a6c3c5e20f91a88cdc8bd3314f867c22494e
RRasiyani <RRasiyani@slb.com>
2021-12-31 16:31:11 -0600

    Install ngrx things, add all components and then add Post model and create entityMetadata, added regular routing for now.

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostsListComponent } from './posts/posts-list/posts-list.component';
+import { SinglePostComponent } from './posts/single-post/single-post.component';
+import { EditPostComponent } from './posts/edit-post/edit-post.component';
+import { AddPostComponent } from './posts/add-post/add-post.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsListComponent,
+    SinglePostComponent,
+    EditPostComponent,
+    AddPostComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
